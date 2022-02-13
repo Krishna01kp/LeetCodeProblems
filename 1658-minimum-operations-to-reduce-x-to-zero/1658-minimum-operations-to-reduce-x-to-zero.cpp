3704e4b022163657dd944b7e1e92e65016c260db
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-02-13 17:02:59 +0530

    Time: 678 ms (19.82%), Space: 168.2 MB (18.87%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -11,7 +11,7 @@ public:
         }
         if(x>sum)   
             return -1;
-        mp[0] = 0;
+        mp[0] = -1;
         
         int len = 0;
         sum-=x;
@@ -21,10 +21,7 @@ public:
             val += nums[i];
             if(mp.count(val-sum))
             {
-                if(val-sum==0)
-                    len = max(len,i-mp[val-sum]+1);
-                else
-                    len = max(len,i-mp[val-sum]);
+                len = max(len,i-mp[val-sum]);
             }
         }
         return len==0?(sum==0?n:-1):n-len;
