7f0350a92d137efa9e4b2615fbc382fb24777f93
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-02-13 16:23:46 +0530

    Time: 814 ms (13.50%), Space: 168.3 MB (15.42%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        int sum = 0;
+        unordered_map<int,int> mp;   
+        for(int i=0;i<n;++i)
+        {
+            sum+=nums[i];
+            mp[sum] = i;
+        }
+        if(x>sum)   
+            return -1;
+        mp[0] = 0;
+        
+        int len = 0;
+        sum-=x;
+        int val = 0;
+        for(int i=0;i<n;++i)
+        {
+            val += nums[i];
+            if(mp.count(val-sum))
+            {
+                if(val-sum==0)
+                    len = max(len,i-mp[val-sum]+1);
+                else
+                    len = max(len,i-mp[val-sum]);
+            }
+        }
+        return len==0?(sum==0?n:-1):n-len;
+    }
+};
