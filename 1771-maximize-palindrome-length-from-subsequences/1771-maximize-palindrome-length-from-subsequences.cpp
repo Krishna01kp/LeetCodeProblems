cfa3172a4cd4a1a3fad785aabd165df7a96af71e
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-03-03 18:52:58 +0530

    Time: 435 ms (18.24%), Space: 24.4 MB (79.25%) - LeetHub

---
+++ 1771-maximize-palindrome-length-from-subsequences/1771-maximize-palindrome-length-from-subsequences.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[2005][2005];
+    int solve(int l, int r,string &s)
+    {
+        if(l>r)return 0;
+        if(l==r)return 1;
+        if(dp[l][r]!=-1)return dp[l][r];
+        int len=0;
+        if(s[l]==s[r])
+        {
+            len=2+solve(l+1,r-1,s);
+        }
+        else
+        {
+            len=max(solve(l+1,r,s),solve(l,r-1,s));
+        }
+        return dp[l][r]=len;
+    }
+    int longestPalindrome(string w1, string w2) {
+        int n=w1.size(),m=w2.size();
+        string s=w1+w2;
+        memset(dp,-1,sizeof(dp));
+        solve(0,n+m-1,s);
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=s.size()-1;j>=0;j--)
+            {
+                if(s[i]==s[j] and (i<n and j>=n))
+                {
+                    ans=max(ans,dp[i][j]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
