7a031208bcd72b1fc94466167bd6614a17837a3c
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-03-14 23:28:17 +0530

    Time: 5 ms (38.32%), Space: 6 MB (80.97%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
+    int fact[15];
+    void solve(int n)
+    {
+        fact[0]=1,fact[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            fact[i]=i*fact[i-1];
+        }
+    }
     string getPermutation(int n, int k) {
-        string ans="";
+        
+        solve(n);
+        vector<int>digits;
         for(int i=1;i<=n;i++)
         {
-            ans+=to_string(i);
+            digits.push_back(i);
         }
+        string ans="";
         
-        while(--k)
+        while(n)
         {
-            next_permutation(ans.begin(),ans.end());
+            int idx= k/fact[n-1];
+            if(k%fact[n-1]==0)idx-=1;
+            ans+=to_string(digits[idx]);
+            k-=fact[n-1]*idx;
+            n--;
+            digits.erase(digits.begin()+idx);
         }
         return ans;
     }
