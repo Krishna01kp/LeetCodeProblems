3eca9fddfa0ca0d81ba9f8487f9bd7734b957c30
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-03-15 00:57:54 +0530

    Time: 428 ms (85.22%), Space: 134.3 MB (32.99%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.cpp
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.cpp
@@ -4,29 +4,24 @@ public:
     int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& req) {
         long long ans=0;
         int n=nums.size();
-        vector<int>mp(n,0);
+        vector<int>fre(n,0);
         for(auto x:req)
         {
-            mp[x[0]]++;
+            fre[x[0]]++;
             if(x[1]+1<n)
-            mp[x[1]+1]--;
+            fre[x[1]+1]--;
         }
-        for(int i=1;i<mp.size();i++)
+        for(int i=1;i<n;i++)
         {
-            mp[i]+=mp[i-1];
+            fre[i]+=fre[i-1];
         }
-        // vector<int>fre;
-        // for(auto m:mp)
-        // {
-        //     if(m>0)
-        //     fre.push_back(m);
-        // }
-        sort(mp.begin(),mp.end());
+      
+        sort(fre.begin(),fre.end());
         sort(nums.begin(),nums.end());
         
         for(int i=0;i<n;i++)
         {
-            long long temp=((long)mp[i]*(long)nums[i]);
+            long long temp=((long)fre[i]*(long)nums[i]);
             temp%=MOD;
             ans+=temp;
             ans%=MOD;
