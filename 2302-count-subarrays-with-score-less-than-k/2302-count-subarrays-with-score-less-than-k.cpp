b641e80a86574af09a1b958e3ffa4ee0b3b0cfb0
Krishna Kant Pandey <66406320+Krishna01kp@users.noreply.github.com>
2022-06-12 08:31:30 +0530

    Time: 217 ms (100.00%), Space: 95 MB (100.00%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -2,32 +2,16 @@ class Solution {
 public:
     #define ll long long 
     long long countSubarrays(vector<int>& nums, long long k) {
-        ll ans=0;
         int n=nums.size();
-        vector<ll>presum(n+1,0);
-        
-        for(int i=1;i<=n;i++)
+        ll ans=0,sum=0;
+        for(int i=0,j=0;j<n;j++)
         {
-            presum[i]=presum[i-1]+nums[i-1];
-        }
-        for(int i=1;i<=n;i++)
-        {
-            ll temp=-1;
-            int l=i,h=n;
-            while(l<=h)
+            sum+=nums[j];
+            while(i<n and (sum*(j-i+1))>=k)
             {
-                int mid=l+(h-l)/2;
-                ll sum=presum[mid]-presum[i-1];
-                sum*=(mid-i+1);
-                if(sum>=k)h=mid-1;
-                else
-                {
-                    temp=mid;
-                    l=mid+1;
-                }
+                sum-=nums[i++];
             }
-            if(temp!=-1)
-            ans+=(temp-i+1);
+            ans+=(j-i+1);
         }
         return ans;
     }
